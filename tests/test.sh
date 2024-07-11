ee42a3a9443f74d5049ce0a21246f2fceea84b35
Ban Hun <banhun@Banui-MacBookPro.local>
2024-07-11 13:59:51 +0900

    test 수정

---
+++ tests/test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Install Ansible Roles."
+ansible-galaxy install --force --ignore-errors -r requirements.yml -p roles
+
+echo "Test Ansible Roles."
+ansible-playbook -i inventory test.yml -e "packages_to_install=['nginx'] packages_to_remove=['httpd']
