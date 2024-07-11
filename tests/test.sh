5929f2be895f266b05a1395985bfc43ca991d150
Ban Hun <banhun@Banui-MacBookPro.local>
2024-07-11 16:10:11 +0900

    저장소 추가

--- tests/test.sh
+++ tests/test.sh
@@ -4,4 +4,4 @@ echo "Install Ansible Roles."
 ansible-galaxy install --force --ignore-errors -r requirements.yml -p roles
 
 echo "Test Ansible Roles."
-ansible-playbook -i inventory test.yml -e "packages_to_install=['net-tools', 'htop'] packages_to_remove=['httpd']"
+ansible-playbook -i inventory test.yml -e "packages_to_install=['net-tools', 'htop']"
